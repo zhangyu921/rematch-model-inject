063038f21373e2697439c4f4e3b40411b5ac6c25
Zhang Yu <zhyuz@outlook.com>
2018-11-19 16:55:00 +0800

    * fix some SyntaxError

---
+++ src/index.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Basil on 2018/10/8.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import hoistNonReactStatics from 'hoist-non-react-statics';
+
+export default models => WrappedComponent => {
+  class ModelComponent extends React.Component {
+    componentWillMount() {
+      const state = this.context.store.getState();
+      for (const m of Object.keys(models)) {
+        if (!state[m]) {
+          this.context.store.model({ name: m, ...models[m] });
+          console.log(`Model injected: ${m}`);
+        }
+      }
+    }
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  }
+
+  ModelComponent.WrappedComponent = WrappedComponent;
+  ModelComponent.contextTypes = {
+    store: PropTypes.object.isRequired,
+  };
+  ModelComponent.displayName = `withModel(${WrappedComponent.displayName ||
+    WrappedComponent.name ||
+    'Component'})`;
+
+  return hoistNonReactStatics(ModelComponent, WrappedComponent);
+};
